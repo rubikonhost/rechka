12f0825031dcacf4c49dfbd124b9151beefbec0d
Fedor <Hucky.Lucky@yandex.ru>
2018-09-03 19:58:19 +0300

    Third Com

--- src/ru/konsantin/io/Main.java
+++ src/ru/konsantin/io/Main.java
@@ -1,7 +1,24 @@
 package ru.konsantin.io;
 
-public class Main {
-    public static void main(String[] args){
+import javax.swing.*;
+import java.awt.*;
 
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        super("Simple Example");
+        this.setBounds(100,100,250,100);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+        Container contentPane = getContentPane();
+        contentPane.add(new JLabel("456"));
+        JButton button = new JButton("123");
+        button.setSize(10,5);
+        contentPane.add(button);
+        setVisible(true);
     }
 }
